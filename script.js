36f49f16ead98ff33c3f48d94a29a472b80f719f
sheikh zayan <sheikhzayan@gmail.com>
2024-08-27 02:03:28 -0700

    Add more logics like some if eles conditions, capitalize first letter , ask confirm and add one more input of (Day)

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 var a = [];
 function save() {
   var todo = document.getElementById("todo").value;
-  if (todo != "") {
+  todo = todo.charAt(0).toUpperCase() + todo.slice(1);
+  var email = document.getElementById('day').value
+  email = email.charAt(0).toUpperCase() + email.slice(1);
+  
+  if (todo == "") {
+    alert("Enter Task..")
+  }
+  else if(email == ""){
+    var con = confirm("Are you sure you want save with day")
+    if(con == true){
+      var obj = {
+        todolist: todo,
+        uEmail: email
+      };
+      a.push(obj);
+      localStorage.setItem("task-list", JSON.stringify(a));
+    }
+    // alert("Enter day..")
+  } 
+  else{
     var obj = {
       todolist: todo,
+      uEmail: email
     };
     a.push(obj);
     localStorage.setItem("task-list", JSON.stringify(a));
     // alert("Save to local storage")
-  } else {
-    alert("Please enter task");
     // return
   }
 }
 function get() {
   var get = JSON.parse(localStorage.getItem("task-list"));
-  var table = "<tr><th></th></tr>";
+  var table = "<tr><th class='fs-5'>Task<hr></th><th class='fs-5'>Day<hr></th></tr>";
   for (let i = 0; i < get.length; i++) {
     table +=
-      "<tr><td><ul id='list'><li>" +
-      get[i].todolist +
-      "<button type='button' class='deletebtn btn-outline-warning' onclick='deleteitem(" +
+      "<tr><td>" +
+      get[i].todolist+ "</td><td>" + get[i].uEmail +"</td><td><button type='button' class='deletebtn btn-outline-warning' onclick='deleteitem(" +
       i +
-      ")'>Delete</button></li></ul><hr></td></tr>";
+      ")'>Delete</button></td></tr>";
   }
   document.getElementById("table-id").innerHTML = table;
 }
